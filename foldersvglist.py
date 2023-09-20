81c3efbe2b79759b162df395b3c6beec2fb24b04
aninovo <aninovo22@gmail.com>
2023-09-20 17:10:26 +0300

    Added a script to form a list of JS objects from whatever is in the folder with images

---
+++ foldersvglist.py
@@ -0,0 +1,9 @@
+import os
+
+folder_path = '/Img'
+subpath = 'misc'
+files = os.listdir(folder_path+'/'+subpath)
+for file in files:
+    if file.endswith('.svg'):
+        sp = file.split('.')[0].split(',')[0];
+        print('{img: "'+subpath + '/' + file + '", name:"' + sp + '"},')
