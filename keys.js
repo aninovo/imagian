376df71be3550c5a33181a2b458f2afc32142470
aninovo <111169680+aninovo@users.noreply.github.com>
2023-09-20 16:44:16 +0300

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,12 @@
+base = 'Img/punct/';
+keys = {};
+keys[' '] = base+'space.svg';
+keys[','] = base+'comma.svg';
+keys['.'] = base+'period.svg';
+keys[':'] = base+'colon.svg';
+keys[';'] = base+'semicolon.svg';
+keys['"'] = base+'open-quote.svg';
+keys['\''] = base+'closing-quote.svg';
+
+keys['?'] = base+'q-mark.svg';
+keys['!'] = base+'ex-mark.svg';
